89719641f6bdb2ed771f170443e77be1c14b9495
1Bodya1 <116078299+1Bodya1@users.noreply.github.com>
2023-12-04 20:35:57 +0200

    created

---
+++ app/src/main/java/com/example/testone/Cat.kt
@@ -0,0 +1,7 @@
+package com.example.testone
+
+data class Cat(
+    val name: String,
+    val description: String,
+    val imageResId: Int
+)
---
+++ app/src/main/java/com/example/testone/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.testone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.VibrationAttributes
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val Bon = Cat("Tom","love him", R.drawable.ic_launcher_background)
+        val Ben = Cat("Kit", "Ukraininan cat", R.drawable.baseline_sanitizer)
+        val Bin = Cat(getString(R.string.cat_name_Bin), getString(R.string.cat_description_Bin), R.drawable.baseline_sanitizer)
+
+        val buttonDetailsBon = findViewById<Button>(R.id.CatTwoDetails)
+        val buttonDetailsBen = findViewById<Button>(R.id.CatOneDetails)
+        val buttonDetailsBin = findViewById<Button>(R.id.CatThreeDetails)
+
+        buttonDetailsBon.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Bon), getString(R.string.cat_description_Bon))
+        }
+        buttonDetailsBen.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Ben), getString(R.string.cat_description_Ben))
+        }
+        buttonDetailsBin.setOnClickListener{
+            LoadDescription(getString(R.string.cat_name_Bin), getString(R.string.cat_description_Bin))
+        }
+    }
+
+    fun LoadDescription(name: String, description: String)
+    {
+        val intent = Intent(this, DescActivity::class.java)
+        intent.putExtra("catName", name)
+        intent.putExtra("catDescription", description)
+        startActivity(intent)
+    }
+
+}
